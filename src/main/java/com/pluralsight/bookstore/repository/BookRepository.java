528d6c917eb82e913e2cd6f7bcb92b902840bdef
amaiza <abdeldjallil.maiza@gmail.com>
2019-02-14 00:46:43 +0100

    add repositoy class

---
+++ src/main/java/com/pluralsight/bookstore/repository/BookRepository.java
@@ -0,0 +1,37 @@
+package com.pluralsight.bookstore.repository;
+
+import com.pluralsight.bookstore.model.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class BookRepository {
+
+    @PersistenceContext(unitName = "bookStorePU")
+    private EntityManager entityManager;
+
+    public Book find(Long id){
+        return entityManager.find(Book.class, id);
+    }
+
+    public Book create(Book book){
+        entityManager.persist(book);
+        return book;
+    }
+
+    public void delete(Long id){
+        entityManager.remove(entityManager.getReference(Book.class, id));
+    }
+
+    public List<Book> findAll(){
+        TypedQuery<Book> bookTypedQuery = entityManager.createQuery("SELECT b from Book b order by b.title desc ", Book.class );
+        return bookTypedQuery.getResultList();
+    }
+
+    public Long countAll(){
+        TypedQuery<Long> longTypedQuery = entityManager.createQuery("select count (b) from Book b" , Long.class);
+        return longTypedQuery.getSingleResult();
+    }
+}
