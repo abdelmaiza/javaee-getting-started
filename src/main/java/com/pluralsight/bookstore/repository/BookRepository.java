d7f0c14ebf7e0f0276e81e939a3690420810a441
amaiza <abdeldjallil.maiza@gmail.com>
2019-02-14 01:51:14 +0100

    adding Transactional

--- src/main/java/com/pluralsight/bookstore/repository/BookRepository.java
+++ src/main/java/com/pluralsight/bookstore/repository/BookRepository.java
@@ -4,9 +4,15 @@ import com.pluralsight.bookstore.model.Book;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Transient;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+@Transactional(SUPPORTS)
 public class BookRepository {
 
     @PersistenceContext(unitName = "bookStorePU")
@@ -16,22 +22,26 @@ public class BookRepository {
         return entityManager.find(Book.class, id);
     }
 
-    public Book create(Book book){
-        entityManager.persist(book);
-        return book;
-    }
-
-    public void delete(Long id){
-        entityManager.remove(entityManager.getReference(Book.class, id));
-    }
-
     public List<Book> findAll(){
         TypedQuery<Book> bookTypedQuery = entityManager.createQuery("SELECT b from Book b order by b.title desc ", Book.class );
         return bookTypedQuery.getResultList();
     }
 
+
     public Long countAll(){
         TypedQuery<Long> longTypedQuery = entityManager.createQuery("select count (b) from Book b" , Long.class);
         return longTypedQuery.getSingleResult();
     }
+
+    @Transactional(REQUIRED)
+    public Book create(Book book){
+        entityManager.persist(book);
+        return book;
+    }
+    @Transactional(REQUIRED)
+    public void delete(Long id){
+        entityManager.remove(entityManager.getReference(Book.class, id));
+    }
+
+
 }
