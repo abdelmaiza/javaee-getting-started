185c6ab1e73d9130080be5b8d60ebc081d704036
Abdeldjallil MAIZA <AMAIZA@externe.generali.fr>
2019-02-18 17:32:18 +0100

    injection CDI

---
+++ javaee-getting-started/src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -0,0 +1,147 @@
+package com.pluralsight.bookstore.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+/**
+ * Created by b010nsm on 12/02/2019.
+ */
+@Entity
+public class Book {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 200)
+    @NotNull
+    @Size(min = 1 , max = 200)
+    private String title;
+
+    @Column(length = 10000)
+    @Size(min = 1 , max = 1000)
+    private String description;
+
+    @Column(name = "unit_cost")
+    @Min(1)
+    private Float unitCost;
+
+    @Column(length = 50)
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String isbn;
+
+    @Column(name = "publication_date")
+    @Temporal(TemporalType.DATE)
+    @Past
+    private Date publicationDate;
+
+    @Column(name = "nb_of_pages")
+    private Integer nbOfPages;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    private Language language;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String a_titile, float unit_cost, int nb_of_pages, Language language, Date date, String s, String description) {
+        this.isbn = isbn;
+        this.title = a_titile;
+        this.unitCost = unit_cost;
+        this.nbOfPages = nb_of_pages;
+        this.language = language;
+        this.publicationDate = date;
+        this.description = description;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNbOfPages() {
+        return nbOfPages;
+    }
+
+    public void setNbOfPages(Integer nbOfPages) {
+        this.nbOfPages = nbOfPages;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override public String toString() {
+        return "Book{" + "id=" + id + ", title='" + title + '\'' + ", description='" + description + '\'' + ", unitCost=" + unitCost
+            + ", isbn='" + isbn + '\'' + ", publicationDate=" + publicationDate + ", nbOfPages=" + nbOfPages + ", imgUrl='" + imgUrl + '\''
+            + ", language=" + language + '}';
+    }
+}
