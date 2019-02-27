7a6643237a5ebdc924b3b7772e4cf7cdad3cd827
amaiza <abdeldjallil.maiza@gmail.com>
2019-02-27 09:18:10 +0100

    restful ws

--- src/main/java/com/pluralsight/bookstore/util/IsbnGenerator.java
+++ src/main/java/com/pluralsight/bookstore/util/IsbnGenerator.java
@@ -10,6 +10,6 @@ public class IsbnGenerator implements NumberGenerator {
     @Override
     public String generateNumber() {
 
-        return "13-5677" + Math.abs(new Random().nextInt());
+        return "13-84356-" + Math.abs(new Random().nextInt());
     }
 }
